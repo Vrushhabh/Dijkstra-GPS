151fecb04b47f9f935595e69201b55cb130ab245
Vrushhabh <vpate81@illinois.edu>
2021-05-02 12:58:07 -0500

    Make text showing you how to manipulate the map and find the shortest path

--- src/graphics/application.cc
+++ src/graphics/application.cc
@@ -13,6 +13,11 @@ namespace graphics {
 void Application::draw() {
     ci::Color8u background_color(200,200,220); // lavenderish color
     ci::gl::clear(background_color);
+    ci::gl::drawStringCentered(
+            "Press c to clear the map. Press f to find the path. Right-click to add blocks. "
+            "Left-click to add weighted spaces",
+            glm::vec2(kWindowSize / 2, kMargin / 2), ci::Color("black"));
+
     map_.DrawMap();
 }
 
@@ -41,7 +46,7 @@ void Application::keyDown(ci::app::KeyEvent event) {
     AppBase::keyDown(event);
     if (event.getCode() == ci::app::KeyEvent::KEY_c) {
         map_.Clear();
-    } else if (event.getCode() == ci::app::KeyEvent::KEY_RETURN) {
+    } else if (event.getCode() == ci::app::KeyEvent::KEY_f) {
         Map map = map_.MakeMap();
         map_.FindShortestPath(map);
     }
